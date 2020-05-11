1b037e62b592c9fd040e6dd39fa33429f8a3043e
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-05-11 17:35:29 +0000

    ++++

--- chat/views.py
+++
@@ -1,9 +0,0 @@
-# chat/views.py
-from django.shortcuts import render
-from django.utils.safestring import mark_safe
-import json
-
-# def home(request):
-#     return render(request, 'room.html')
-
-
